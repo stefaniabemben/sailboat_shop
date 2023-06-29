c58e67f8035a4fae743491e2f9c8b5563c0f422f
stefaniabemben <137799832+stefaniabemben@users.noreply.github.com>
2023-06-29 11:30:15 +0200

    Update sailboat.js

--- sailboat.js
+++ sailboat.js
@@ -1,1 +1,11 @@
-res.end(`<html><body><h1>This is HTML</h1></body></html>`);
+const http = require('http');
+const port = process.env.PORT || 8080;
+
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Grupa ZDTESTpol127 jest super SUPER!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(port);
